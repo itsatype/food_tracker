ccd74bd6ec2d3398d83fffd5ae57b959c383ae0a
Leah Einhorn <leah@Leahs-iMac.home>
2016-11-10 00:44:12 -0500

    add models migration

--- food/models.py
+++ food/models.py
@@ -2,4 +2,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Food(models.Model):
+	 food_item = models.CharField(max_length=200)
+   upc = models.IntegerField(max_length=16)
+   food_item = models.CharField(max_length=200)
+   image = models.CharField(max_length=200)
+
+class User(models.Model):
+	name = models.CharField(max_length=200)
+  pin = models.IntegerField(max_length=4)   
+  company =  models.ForeignKey(Company, on_delete=models.CASCADE)
+
+   
+class Order(models.Model):
+  user =  models.ForeignKey(User, on_delete=models.CASCADE)
+  food = models.ForeignKey(Food, on_delete=models.CASCADE)
+  quantity = models.IntegerField(max_length=3)
+
+
+class Company(models.Model):
+   name = models.CharField(max_length=200)
